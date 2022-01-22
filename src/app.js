0d27ca5814da67d29cc264e19a66c4701e28ae8c
Balasubramanian S M <balabsm@outlook.com>
2022-01-22 21:56:30 +0530

    Mod 2

--- src/app.js
+++ src/app.js
@@ -8,9 +8,29 @@ app.set('views',path.join(__dirname,'/views'));
 app.set('view engine','ejs');
 
 app.use(express.static(path.join(__dirname,'/public')));
+console.log(__dirname);
+const accountData = fs.readFileSync(__dirname+'/json/accounts.json',"utf8");
+const accounts=JSON.parse(accountData);
+
+const userData = fs.readFileSync(__dirname+'/json/users.json',"utf8");
+const users=JSON.parse(userData);
 
 app.get('/',function(req,res){
-    res.render('index',{title:'Index'})
+    res.render('index',{title:'Account Summary',accounts:accounts})
+})
+
+app.get('/savings',function(req,res){
+    res.render('account',{account:accounts.savings})
+})
+app.get('/checking',function(req,res){
+    res.render('account',{account:accounts.checking})
+})
+app.get('/credit',function(req,res){
+    res.render('account',{account:accounts.credit})
+})
+
+app.get('/profile',function(req,res){
+    res.render('profile',{user: users[0]})
 })
 
 app.listen(3000,function(){
